075195943cc091983e85652135eeb16f854a82ec
Tiny-Quant <tay.42@gmail.com>
2026-03-24 03:50:16 -0500

    Added send test function.

--- plugin/quadHead.lua
+++ plugin/quadHead.lua
@@ -8,3 +8,9 @@ vim.g.loaded_quadHead = true
 vim.api.nvim_create_user_command("QuadHeadTest", function()
   print("quadHead loaded")
 end, {})
+
+vim.api.nvim_create_user_command("QuadHeadSendTest", function()
+  local b = require("quadHead.backend").get()
+
+  b.send(0, "x <- 1\nx\n")
+end, {})
