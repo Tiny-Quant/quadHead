3a123cedcfef0f452bc5388a66afe7b26c470ba2
Tiny-Quant <tay.42@gmail.com>
2026-03-24 03:46:03 -0500

    Added wezterm send function.

--- lua/quadHead/backend/wezterm.lua
+++ lua/quadHead/backend/wezterm.lua
@@ -1,7 +1,25 @@
 local M = {}
 
+local function system_with_input(cmd, input)
+  return vim.fn.system(cmd, input)
+end
+
+
 function M.send(pane, text)
-  error("wezterm.send not implemented")
+  local cmd = {
+    "wezterm",
+    "cli",
+    "send-text",
+    "--no-paste",
+    "--pane_id",
+    tostring(pane)
+  }
+
+  local result = system_with_input(cmd, text)
+
+  if vim.v.shell_error ~= 0 then
+    error(result)
+  end
 end
 
 function M.split(cmd, cwd)
